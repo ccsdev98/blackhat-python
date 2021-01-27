1a12476e49b4a3662defced940c66470a321e5ae
ccsdev98 <chaitanyachavan118@gmail.com>
2021-01-27 16:24:51 +0530

    tcp-server created

---
+++ tcp-server.py
@@ -0,0 +1,32 @@
+import socket
+import threading
+
+ip = "0.0.0.0"
+port = 9999
+
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+server.bind((ip,port))
+
+server.listen(5)
+
+print ("[*] Welcome to server,you now connected to %s:%d" % (ip,port))
+
+def handle_client(client_socket):
+    request = client_socket.recv(1024)
+
+    print ("Recieved request %s"%request)
+
+    #send the packet back to client
+    client_socket.send("ACK!..")
+
+    client_socket.close()
+
+while True:
+    client,addr = server.accept()
+
+    print ("[*] Accepted connection from %s:%d" % (addr[0],addr[0]))
+
+    #spin up our client thread to handle incomming data 
+    client_handler = threading.thread(target=handle_client,args=(client,))
+    client_handler.start()
